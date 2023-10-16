b5ce4071dbad26324b3e35097fb702f08ab7164c
David Ramos <davidramos.13@outlook.com>
2023-10-16 14:18:56 +0200

    small podcast view style update

--- src/components/PodcastView/PodcastView.tsx
+++ src/components/PodcastView/PodcastView.tsx
@@ -13,8 +13,8 @@ import { createPlayList } from '~/entities/playItem';
 
 const BackButton = tw(IconButton)`bg-bgGray rounded-[15px]`;
 const DivImage = tw.div`w-full h-[280px] mt-[22px] bg-podcast bg-cover bg-no-repeat bg-center rounded-[15px]`;
-const DivTitleContainer = tw.div`grid grid-cols-podcastView`;
-const TitleText = tw(Typography)`text-2xl text-center text-white font-bold`;
+const DivTitleContainer = tw.div`grid grid-cols-podcastView gap-2 lg:-mb-16 lg:gap-24`;
+const TitleText = tw(Typography)`text-[32px] text-center text-white font-bold`;
 const StyledPlayerCell = tw(PlayerCell)`h-[60px] w-[60px]`;
 
 const PodcastView = () => {
